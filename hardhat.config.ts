fa463ea8bfa9e51ecf15ef889b866533f74f5321
kubr7 <kubrtech@gmail.com>
2025-04-07 02:46:37 +0000

    hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,9 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@typechain/hardhat";
-import * as dotenv from "dotenv";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
